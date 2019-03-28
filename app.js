5b44feee28179e81bec95d72c13077aff583d35e
Leonardo Guedez <leolespaul@gmail.com>
2019-03-28 18:41:22 -0400

    Adds post endpoint and callback for error handling

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 const fcm = require('./fcm');
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post('/push_notification', (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageUrl = req.body.image_url;
+
+    console.log('title =', title);
+    console.log('desrip =', description);
+    console.log('url =', imageUrl);
+
     fcm.sendPushNotification({
-        title: 'Se ha generado una alerta en el sistema',
-        description: 'Se ha detectado un objeto buscado para el que se establecieron condiciones',
-        image_url: 'https://www.spirit-of-metal.com/les%20goupes/O/Opeth/pics/359786_logo.jpg'
+        title: title,
+        description: description,
+        image_url: imageUrl
+    }, function(err, result) {
+        if (err) {
+            console.log('ERROR: Could not send push notification:', err);
+            return res.status(500).json({'message': 'Could not send push notification', error: err});
+        }
+
+        res.status(200).json({ result: result });
     });
-    res.sendStatus(200);
-});
+})
 
 
 const PORT = process.env.PORT || 3000;
--- fcm.js
+++ fcm.js
@@ -8,7 +8,7 @@ admin.initializeApp({
 
 const registrationToken = 'cddkRf4UKNk:APA91bFczVQ7ETw42BfE0vtbMoAdYdn2i3ug54jz-LrPFyA1Aa7_jC4BMllRaBw8gSFZNTltVN38xmbzymesITHxcqUTYm0rso4dC3HSR27iPGIeNqauneIFEBLuxxGo87DbMYy4z2nV'
 
-function sendPushNotification(data) {
+function sendPushNotification(data, cb) {
     const notification = {
         token: registrationToken,
         data: data
@@ -16,9 +16,11 @@ function sendPushNotification(data) {
 
     admin.messaging().send(notification)
         .then((response) => {
+            cb(null, response);
             console.log('Succesfully sent message:', response);
         })
         .catch((err) => {
+            cb(err, null);
             console.log('Error sending message:', err);
         });
 }
